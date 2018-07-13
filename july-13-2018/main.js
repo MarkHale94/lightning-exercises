0366c5a9a6d74af8ab079870825d42e67a4e5edf
Mark Hale <mark.hale94@gmail.com>
2018-07-13 12:43:11 -0500

    added more stuff

--- july-13-2018/main.js
+++ july-13-2018/main.js
@@ -26,3 +26,56 @@ go("up", 19000);
 go("down", 12);
 
 
+
+
+
+
+// Given the following set of data about chicken birth rates, you need to filter out the objects where the birth rate value is greater than 15 per 1000 chickens for that year. Store the good years in a new array named `bestYears`.
+
+const birthRates = [
+    { year: 1969, birthRate: 13}, 
+    { year: 1970, birthRate: 16}, 
+    { year: 1971, birthRate: 15}, 
+    { year: 1972, birthRate: 11}, 
+    { year: 1973, birthRate: 18}, 
+    { year: 1974, birthRate: 17}, 
+    { year: 1975, birthRate: 9}
+  ];
+
+const bestYears = [];
+
+// for (let i=0; i<birthRates.legnth; i++){
+//     const currentYear = birthRates[i];
+//     if(currentYear.birthRate>15){
+//         bestYears.push(currentYear);
+//     }
+
+// }
+
+// console.log(bestYears);
+
+
+// birthRates.forEach(function(currentYear){
+//     if( currentYear.birthRate>15){
+//       bestYears.push(currentYear);
+//     }
+// });
+
+// for (const currentYear of birthRates) {
+//     if(currentYear.birthRate>15){
+//         bestYears.push(currentYear);
+//     }
+// }
+
+// console.log(bestYears);
+
+
+// const politician = {
+//     platform: {
+//         money: ["I like it", "I have lots of it", "I don't want other people to have any"],
+//         health: "I'm against it"
+//     },
+//     district: "Michigan 13"
+// }
+
+// politician.platform.money
